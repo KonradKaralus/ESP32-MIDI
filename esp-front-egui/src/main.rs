4f4352e374797a641e1b1659ea8ada7229232b52
KonradKaralus <g78285423@gmail.com>
2024-08-23 15:28:05 +0200

    changed layout, reverse aliases

--- esp-front-egui/src/main.rs
+++ esp-front-egui/src/main.rs
@@ -1,27 +1,30 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 pub mod utils;
 
-use std::{collections::HashMap, iter, sync::{Arc, Mutex}};
+use eframe::egui::{self, vec2, Label, Pos2, Style, TextEdit};
 use indexmap::IndexMap;
-use eframe::egui::{self, vec2, Button, Label, Pos2, Style, TextEdit};
-use io_bluetooth::bt::{self, BtStream};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+use utils::{get_connection, try_connect, Connection};
 
-const ADDRESS:&str = "78:21:84:8c:71:2a";
+pub const ADDRESS: &str = "78:21:84:8c:71:2a";
 
-const NUM_PEDALS:u8 = 6; 
+const NUM_PEDALS: u8 = 6;
 
 fn main() -> Result<(), eframe::Error> {
-
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-        .with_inner_size([1000.0, 690.0])
-        .with_resizable(false)
-        .with_position(Pos2::new(100f32, 100f32)),
+            .with_inner_size([1000.0, 690.0])
+            .with_resizable(false)
+            .with_position(Pos2::new(100f32, 100f32)),
         ..Default::default()
     };
     eframe::run_native(
-
         "My egui App",
         options,
         Box::new(|_cc| {
@@ -38,72 +41,64 @@ fn main() -> Result<(), eframe::Error> {
 
 struct MyApp {
     columns: Arc<Mutex<IndexMap<u8, String>>>,
-    socket: Option<BtStream>,
-    custom_cmd:String,
-    custom_pedal_nr:String,
-    tempo:String,
-    tempo_list:String,
-    aliases:HashMap<String,String>
+    _tempo: String,
+    aliases: HashMap<String, String>,
+    aliases_rev: HashMap<String, String>,
+    connection: Connection,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        let loaded_config: IndexMap<u8,String> = IndexMap::with_capacity(NUM_PEDALS as usize); 
+        let loaded_config: IndexMap<u8, String> = IndexMap::with_capacity(NUM_PEDALS as usize);
 
-        let devices = bt::discover_devices().unwrap();
-        let mut device_idx=0;
-        for (idx,device) in devices.iter().enumerate() {
-            if *device.to_string() == ADDRESS.to_string() {
-                device_idx = idx;
-                break;
-            }
-        }
+        let c = get_connection();
+        let c2 = c.clone();
 
-        if devices.len() == 0 {
-            panic!("no matching device");
-        }
+        thread::spawn(|| {
+            try_connect(c2);
+        });
 
-        let socket = BtStream::connect(iter::once(&devices[device_idx]), bt::BtProtocol::RFCOMM).unwrap();
+        let aliases = MyApp::get_aliases();
 
-        match socket.peer_addr() {
-            Ok(_) => {},
-            Err(err) => println!("An error occured while retrieving the peername: {:?}", err),
-        }
-
-        match socket.local_addr() {
-            Ok(_) => {},
-            Err(err) => println!("An error occured while retrieving the sockname: {:?}", err),
-        }
-
-        let mut res = Self {
+        let res = Self {
             columns: Arc::new(Mutex::new(loaded_config)),
-            socket: Option::from(socket),
-            custom_cmd:"".to_string(),
-            custom_pedal_nr:"".to_string(),
-            tempo:"".to_string(),
-            tempo_list:"".to_string(),
-            aliases: MyApp::get_aliases()
+            _tempo: "".to_string(),
+            aliases: aliases.0,
+            aliases_rev: aliases.1,
+            connection: c,
         };
 
-        res.req_cfg();        
+        // res.req_cfg();
         res
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.request_repaint_after(Duration::from_millis(250));
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.columns(2, |columns| {
-                
-                for (i,str) in self.columns.lock().unwrap().iter_mut() {
-                    columns[0].add_sized(
-                        vec2(20.0,20.0),
-                        Label::new(i.to_string()));
-                    columns[1].add_sized(
-                        vec2(40.0,20.0),
-                        TextEdit::singleline(str));
-                }
+            if self.is_newly_connected() {
+                println!("req");
+                self.req_cfg();
+            }
 
+            ui.horizontal(|ui| {
+                ui.label(self.get_connection_status());
+                if !self.is_connected() {
+                    if ui.button("Connect").clicked() {
+                        let c2 = self.connection.clone();
+                        thread::spawn(|| {
+                            try_connect(c2);
+                        });
+                    }
+                }
+            });
+
+            ui.columns(2, |columns| {
+                for (i, str) in self.columns.lock().unwrap().iter_mut() {
+                    columns[0].add_sized(vec2(20.0, 20.0), Label::new(i.to_string()));
+                    columns[1].add_sized(vec2(40.0, 20.0), TextEdit::singleline(str));
+                }
             });
 
             ui.horizontal(|ui| {
@@ -119,37 +114,6 @@ impl eframe::App for MyApp {
                 if ui.button("Get").clicked() {
                     self.req_cfg();
                 }
-            });
-
-            ui.separator();
-
-            ui.horizontal(|ui| {
-                if ui.add(Button::new("Tempos").min_size(vec2(80f32, 20f32))).clicked() {
-                    self.send_tempo_list();
-                }
-                ui.add(TextEdit::singleline(&mut self.tempo_list))
-            });
-
-            ui.horizontal(|ui| {
-                if ui.add(Button::new("Tempo").min_size(vec2(80f32, 20f32))).clicked() {
-                    self.send_tempo_change();
-                }
-                ui.add(TextEdit::singleline(&mut self.tempo))
-            });
-            ui.horizontal(|ui| {
-
-                if ui.add(Button::new("Hit Pedal").min_size(vec2(80f32, 20f32))).clicked() {
-                    self.send_pedal_command();
-                }
-
-                ui.add(TextEdit::singleline(&mut self.custom_pedal_nr))
-            });
-
-            ui.horizontal(|ui| {
-                if ui.add(Button::new("Command").min_size(vec2(80f32, 20f32))).clicked() {
-                    self.send_midi_command();
-                }
-                ui.add(TextEdit::singleline(&mut self.custom_cmd))
             });
         });
     }
--- esp-front-egui/src/utils.rs
+++ esp-front-egui/src/utils.rs
@@ -1,161 +1,245 @@
-use std::{collections::HashMap, fs::File};
+use std::{collections::HashMap, fs::File, iter, sync::Arc};
 
+use eframe::egui::mutex::Mutex;
 use indexmap::IndexMap;
+use io_bluetooth::bt::{self, BtStream};
 use native_dialog::FileDialog;
 
-use crate::{MyApp, NUM_PEDALS};
+use crate::{MyApp, ADDRESS, NUM_PEDALS};
+
+const CONNECTION_ATTEMPTS: usize = 3;
+
+const ALIASES: [(&str, &str); 4] = [
+    ("Down", "CC52"),
+    ("Up", "CC53"),
+    ("Tune", "CC68"),
+    ("Tap", "CC64"),
+];
+
+pub struct IConnection {
+    pub socket: Option<BtStream>,
+    pub status: String,
+    pub new_connect: bool,
+}
+
+pub type Connection = Arc<Mutex<IConnection>>;
+
+impl Default for IConnection {
+    fn default() -> Self {
+        Self {
+            socket: Option::None,
+            status: "Not connected".to_string(),
+            new_connect: false,
+        }
+    }
+}
+
+pub fn get_connection() -> Connection {
+    Arc::new(Mutex::new(IConnection::default()))
+}
+
+pub fn try_connect(connection: Connection) {
+    connection.lock().status = "Connecting".to_string();
+
+    for i in 0..CONNECTION_ATTEMPTS {
+        let mut devices = bt::discover_devices().unwrap();
+
+        devices = devices
+            .into_iter()
+            .filter(|d| *d.to_string() == ADDRESS.to_string())
+            .collect();
+
+        if devices.len() == 1 {
+            let socket =
+                BtStream::connect(iter::once(&devices[0]), bt::BtProtocol::RFCOMM).unwrap();
+
+            match socket.peer_addr() {
+                Ok(_) => {}
+                Err(err) => println!("An error occured while retrieving the peername: {:?}", err),
+            }
+
+            match socket.local_addr() {
+                Ok(_) => {}
+                Err(err) => println!("An error occured while retrieving the sockname: {:?}", err),
+            }
+
+            connection.lock().socket = Option::from(socket);
+            connection.lock().status = "Connected".to_string();
+            connection.lock().new_connect = true;
+            return;
+        }
+
+        // sleep(Duration::from_millis(1500));
+        connection.lock().status = format!("{}. Attempt failed", i + 1);
+    }
+
+    connection.lock().status = "Connection failed".to_string();
+}
 
 impl MyApp {
     pub fn _print_current_cfg(&mut self) {
         println!("current cfg: {:?}", *self.columns.lock().unwrap());
     }
 
+    pub fn is_connected(&self) -> bool {
+        self.connection.lock().socket.is_some()
+    }
+
+    pub fn is_newly_connected(&self) -> bool {
+        if self.connection.lock().new_connect {
+            self.connection.lock().new_connect = false;
+            return true;
+        }
+        false
+    }
+
     pub fn _cfg_str(&self) -> String {
         format!("current cfg: {:?}", *self.columns.lock().unwrap())
     }
-    
-    
-    pub fn req_cfg(&mut self) {
-        let cfg_req:Vec<u8> = vec![0x00];
 
-        let socket = match &self.socket {
+    pub fn get_connection_status(&self) -> String {
+        self.connection.lock().status.clone()
+    }
+
+    pub fn req_cfg(&mut self) {
+        let cfg_req: Vec<u8> = vec![0x00];
+
+        let connection = self.connection.clone();
+        let s = &connection.lock().socket;
+
+        let socket = match s {
             None => return,
-            Some(s) => s
+            Some(s) => s,
         };
-    
         socket.send(&cfg_req).unwrap();
-        
-        let mut cfg_buffer:Vec<u8> = vec![0; (2*NUM_PEDALS + 1) as usize];
-    
+
+        let mut cfg_buffer: Vec<u8> = vec![0; (2 * NUM_PEDALS + 1) as usize];
+
         socket.recv(&mut cfg_buffer).unwrap();
-    
+
         cfg_buffer.pop();
-    
+
         let mut index = 0;
 
         let mut loaded_config = self.columns.lock().unwrap();
-    
+
         loop {
             let ped = cfg_buffer[index];
-            let value = Self::cfg_str_from_value(cfg_buffer[index+1]);
-    
+            let value = self.cfg_str_from_value(cfg_buffer[index + 1]);
+
             if value.is_empty() {
                 panic!("was not CC or PC")
             }
 
-            match self.match_alias(value.clone()) {
-                Some(s) => loaded_config.insert(ped, s),
-                None => loaded_config.insert(ped, value)
-            };
-    
-            
+            loaded_config.insert(ped, value);
+
             index += 2;
-            if index as u8 >= NUM_PEDALS*2 {
+            if index as u8 >= NUM_PEDALS * 2 {
                 break;
             }
         }
         drop(loaded_config);
         self.sort_cfg();
     }
-    
-    
-    fn cfg_str_from_value(value:u8) -> String {
-    
+
+    fn cfg_str_from_value(&self, value: u8) -> String {
         let mut type_st = "".to_string();
         let mut input = value;
-    
+
         let msg_type = input & 0x80;
         match msg_type {
             0 => type_st += "PC",
-            1 => type_st += "CC",
+            128 => type_st += "CC",
             _ => {}
-        }  
-    
-        input = input & 0x7F;  
-        type_st += &(input as i32).to_string();  
-    
+        }
+
+        input = input & 0x7F;
+        type_st += &(input as i32).to_string();
+
+        match self.match_alias_rev(&type_st) {
+            Some(s) => type_st = s.clone(),
+            _ => {}
+        }
+
         type_st
     }
 
-    fn command_from_str(&self, cmd:&String) -> Option<u8> {
-            let mut num_value:u8;
+    fn command_from_str(&self, cmd: &String) -> Option<u8> {
+        let mut num_value: u8;
 
-            let input:String;
+        let input: String;
 
-            match self.match_alias(cmd.clone()) {
-                Some(n) => input = n,
-                None => input = cmd.clone()
-            }
-            
-            if input.contains("CC") {
-                let value = input.replace("CC", "");
-                num_value = value.parse::<u8>().unwrap();
-                num_value += 128; //set first bit
-            }
-            else if input.contains("PC") {
-                let value = input.replace("PC", "");
-                num_value = value.parse::<u8>().unwrap();  
-            }
-            else {
-                return Option::None;
-            }
+        match self.match_alias(&cmd) {
+            Some(n) => input = n.clone(),
+            None => input = cmd.clone(),
+        }
 
-            Option::from(num_value)
+        if input.contains("CC") {
+            let value = input.replace("CC", "");
+            num_value = value.parse::<u8>().unwrap();
+            num_value += 128; //set first bit
+        } else if input.contains("PC") {
+            let value = input.replace("PC", "");
+            num_value = value.parse::<u8>().unwrap();
+        } else {
+            return Option::None;
+        }
+
+        Option::from(num_value)
     }
 
     pub fn serialize_cfg(&mut self) {
-
         let input = FileDialog::new()
-        .set_location("~/Documents")
-        .add_filter(".json", &["json"])
-        .show_save_single_file()
-        .unwrap();
-    
+            .set_location("~/Documents")
+            .add_filter(".json", &["json"])
+            .show_save_single_file()
+            .unwrap();
+
         let path;
-    
+
         match input {
             None => return,
-            Some(p) => path = p
+            Some(p) => path = p,
         }
-    
+
         let file = match File::create(&path) {
             Err(why) => panic!("couldn't open {}", why),
             Ok(file) => file,
         };
 
-        let mut cfg:std::collections::HashMap<u8,String> = HashMap::new();
-        self.columns.lock().unwrap().iter().for_each(|(k,v)| {
+        let mut cfg: std::collections::HashMap<u8, String> = HashMap::new();
+        self.columns.lock().unwrap().iter().for_each(|(k, v)| {
             cfg.insert(*k, v.clone());
         });
-        cfg.insert(0xFF, self.tempo_list.clone());
+        // cfg.insert(0xFF, self.tempo_list.clone());
         serde_json::to_writer(file, &cfg).unwrap();
     }
-    
+
     pub fn load_cfg(&mut self) {
         let input = FileDialog::new()
-        .set_location("~/Documents")
-        .add_filter(".json", &["json"])
-        .show_open_single_file()
-        .unwrap();
-    
+            .set_location("~/Documents")
+            .add_filter(".json", &["json"])
+            .show_open_single_file()
+            .unwrap();
+
         let path;
-    
+
         match input {
             None => return,
-            Some(p) => path = p
+            Some(p) => path = p,
         }
-    
+
         let file = match File::open(&path) {
             Err(why) => panic!("couldn't open {}", why),
             Ok(file) => file,
         };
-    
-        let mut cfg:std::collections::HashMap<u8, String> = serde_json::from_reader(file).unwrap();
+
+        let cfg: std::collections::HashMap<u8, String> = serde_json::from_reader(file).unwrap();
         let mut res = IndexMap::new();
 
-        self.tempo_list = cfg.remove(&0xFF).unwrap();
-        cfg.iter().for_each(|(k,v)| {
-            res.insert(*k,v.clone());
+        // self.tempo_list = cfg.remove(&0xFF).unwrap();
+        cfg.iter().for_each(|(k, v)| {
+            res.insert(*k, v.clone());
         });
 
         *self.columns.lock().unwrap() = res;
@@ -164,117 +248,103 @@ impl MyApp {
     }
 
     pub fn send_cfg(&mut self) {
-
-        let mut output_buffer:Vec<u8> = Vec::with_capacity((NUM_PEDALS*2 + 1) as usize);
+        let mut output_buffer: Vec<u8> = Vec::with_capacity((NUM_PEDALS * 2 + 1) as usize);
         output_buffer.push(0x01);
 
         let cfg = self.columns.lock().unwrap();
-    
+
         for (pedal, input) in cfg.iter() {
             let num_value = self.command_from_str(input).unwrap();
-    
+
             output_buffer.push(*pedal);
             output_buffer.push(num_value);
         }
         output_buffer.push(0x00);
 
-        self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
-
+        self.connection
+            .lock()
+            .socket
+            .as_ref()
+            .unwrap()
+            .send(&output_buffer)
+            .unwrap();
         drop(cfg);
     }
 
-    pub fn send_midi_command(&mut self) {
-        let mut output_buffer:Vec<u8> = Vec::with_capacity((NUM_PEDALS*2 + 1) as usize);
-        output_buffer.push(0x02);
+    // pub fn _send_midi_command(&mut self) {
+    //     let mut output_buffer: Vec<u8> = Vec::with_capacity((NUM_PEDALS * 2 + 1) as usize);
+    //     output_buffer.push(0x02);
 
-        let command = self.command_from_str(&self.custom_cmd).unwrap();
+    //     let command = self.command_from_str(&self.custom_cmd).unwrap();
 
-        output_buffer.push(command);
+    //     output_buffer.push(command);
 
-        self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
-    }
+    //     self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
+    // }
 
-    pub fn send_pedal_command(&mut self) {
-        let mut output_buffer:Vec<u8> = Vec::with_capacity((NUM_PEDALS*2 + 1) as usize);
-        output_buffer.push(0x03);
+    // pub fn _send_pedal_command(&mut self) {
+    //     let mut output_buffer: Vec<u8> = Vec::with_capacity((NUM_PEDALS * 2 + 1) as usize);
+    //     output_buffer.push(0x03);
 
-        output_buffer.push(self.custom_pedal_nr.parse().unwrap());
+    //     output_buffer.push(self.custom_pedal_nr.parse().unwrap());
 
-        self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
-    }
+    //     self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
+    // }
 
-    pub fn send_tempo_change(&mut self) {
-        let mut output_buffer:Vec<u8> = Vec::with_capacity((NUM_PEDALS*2 + 1) as usize);
-        output_buffer.push(0x04);
+    // pub fn _send_tempo_change(&mut self) {
+    //     let mut output_buffer: Vec<u8> = Vec::with_capacity((NUM_PEDALS * 2 + 1) as usize);
+    //     output_buffer.push(0x04);
 
-        output_buffer.append(&mut Self::tempo_bytes_from_str(&self.tempo));
+    //     output_buffer.append(&mut Self::tempo_bytes_from_str(&self.tempo));
 
-        self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
-    }
-
-    pub fn send_tempo_list(&mut self) {
-        let mut output_buffer:Vec<u8> = Vec::new();
-        output_buffer.push(0x05);
-
-        let tempos:Vec<&str> = self.tempo_list.split(",").collect();
-
-        for tempo in tempos {
-            output_buffer.append(&mut Self::tempo_bytes_from_str(tempo));
-        }
-        for _ in 0..5 {
-            output_buffer.push(0x00);
-        }
-        println!("sending tempolist: {:?}", output_buffer);
-        self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
-    }
+    //     self.socket.as_ref().unwrap().send(&output_buffer).unwrap();
+    // }
 
     fn sort_cfg(&mut self) {
-        let mut new_cfg:IndexMap<u8, String> = IndexMap::new();
+        let mut new_cfg: IndexMap<u8, String> = IndexMap::new();
 
-        let mut collect:Vec<(u8, String)> = vec![];
+        let mut collect: Vec<(u8, String)> = vec![];
 
-        self.columns.lock().unwrap().iter().for_each(|(k,v)| {
-            collect.push((*k,v.clone()))
+        self.columns
+            .lock()
+            .unwrap()
+            .iter()
+            .for_each(|(k, v)| collect.push((*k, v.clone())));
+
+        collect.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+        collect.iter().for_each(|(k, v)| {
+            new_cfg.insert(*k, v.clone());
         });
-
-        collect.sort_by(|a,b| a.0.partial_cmp(&b.0).unwrap());
-
-        collect.iter().for_each(|(k,v)| {
-            new_cfg.insert(*k,v.clone());
-        });   
 
         *self.columns.lock().unwrap() = new_cfg;
     }
 
-    fn match_alias(&self, input:String) -> Option<String> {
-
-        for (a,b) in &self.aliases {
-            if *a == input {
-                return Option::from(b.clone());
-            }
-            if *b == input {
-                return Option::from(a.clone());
-            }
-        }
-        return Option::None;
+    fn match_alias(&self, input: &String) -> Option<&String> {
+        self.aliases.get(input)
     }
 
-    fn tempo_bytes_from_str(input:&str) -> Vec<u8> {
-        let f_value:f32 = input.parse().unwrap();
+    fn match_alias_rev(&self, input: &String) -> Option<&String> {
+        self.aliases_rev.get(input)
+    }
+
+    fn tempo_bytes_from_str(input: &str) -> Vec<u8> {
+        let f_value: f32 = input.parse().unwrap();
         let mut res = vec![];
         f_value.to_le_bytes().iter().for_each(|b| res.push(*b));
 
         res
     }
 
-    pub fn get_aliases() -> HashMap<String, String> {
+    pub fn get_aliases() -> (HashMap<String, String>, HashMap<String, String>) {
         let mut res = HashMap::new();
-        res.insert("down".into(), "CC52".into());
-        res.insert("up".into(), "CC53".into());
-        res.insert("tun".into(), "CC68".into());
-        res.insert("tnext".into(), "CC127".into());
-        res.insert("T".into(), "CC64".into());
+        let mut res2 = HashMap::new();
 
-        res
+        for (key, val) in ALIASES {
+            res.insert(key.to_string(), val.to_string());
+            res2.insert(val.to_string(), key.to_string());
+        }
+
+        (res, res2)
     }
 }
